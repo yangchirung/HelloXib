cfc2c2dd576a096aee24faa8ef5f792697f3a20a
jackyang <jy6231@yahoo.com.tw>
2019-11-30 16:56:56 +0800

    Initial Commit

---
+++ HelloXib/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloXib
+//
+//  Created by MBP_JackYang on 2019/11/30.
+//  Copyright © 2019 MBP_JackYang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
